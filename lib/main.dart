1a83664da5017268ac9edd680a90d7f3ae46da4a
saki8661 <saki8661@naver.com>
2023-10-04 15:43:44 +0900

    í플러터 recipe_app 클론코딩

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import 'pages/recipe_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: "PatuaOne"),
+      home: RecipePage(),
+    );
+  }
+}
